df8e12d07ade5d4630bdbc858bb45486e9c1daf8
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2022-11-24 11:15:09 +0200

    1st commit

--- js/script.js
+++ js/script.js
@@ -52,8 +52,8 @@
 	});
 
 	app.config(function ($routeProvider, $locationProvider) {
-		//$locationProvider
-			//.html5Mode(true)
+		$locationProvider
+			.html5Mode(true)
 			//.hashPrefix('');
 
 		$routeProvider
